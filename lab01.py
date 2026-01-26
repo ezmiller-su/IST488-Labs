c63dc0ff56c840b7332008d27de586c9e57865dd
ezmiller-su <166886827+ezmiller-su@users.noreply.github.com>
2026-01-26 19:13:39 +0000

    nav and secrets

---
+++ lab01.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("Document Q&A")
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+if openai_api_key:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via `st.text_area`.
+    question = st.text_area(
+        "What do you want to know about the document?",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Scrutinize this document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-5-nano",
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
+
+else:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
