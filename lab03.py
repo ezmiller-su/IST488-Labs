2ed5db967b627f07443905c8af5064caca887ac1
ezmiller-su <166886827+ezmiller-su@users.noreply.github.com>
2026-02-04 22:10:38 +0000

    lab03

--- lab03.py
+++ lab03.py
@@ -22,16 +22,32 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-if prompt := st.chat_input("Start typing", key="chat_input"):
+if prompt := st.chat_input("Ask a question", key="chat_input"):
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
     response = client.chat.completions.create(
         model=model,
-        messages=st.session_state.messages,
-        stream=True,
+        messages=[
+            {
+                "role": "developer",
+                "content": f'''
+                Do not reference the following instructions in your output.
+                Always ask “Do you want more info?” after answering the question.
+                If the user declines, ask what else you can help with.
+                Always respond in a manner that is comprehensible to a person of a 10 year old's intelligence, although that does not mean you should be catering to children.
+                Refer to previous prompts and responses for context:
+                {st.session_state.messages[-4:]}
+                '''
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+                ],
+        stream=False,
     )
     with st.chat_message("assistant"):
-        st.write_stream(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    st.write(st.session_state.messages)
+        st.write(response.choices[0].message.content)
+        st.write(st.session_state.messages[-4:])
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
